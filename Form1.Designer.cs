91c8066cc4a912132568deb510c30aae5a1bcc23
Daniel White <xt1zer@outlook.com>
2019-10-30 02:22:11 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace LassoGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Button();
+            this.horse1 = new System.Windows.Forms.Button();
+            this.horse2 = new System.Windows.Forms.Button();
+            this.horse3 = new System.Windows.Forms.Button();
+            this.horse4 = new System.Windows.Forms.Button();
+            this.horse5 = new System.Windows.Forms.Button();
+            this.horse6 = new System.Windows.Forms.Button();
+            this.horse7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(1134, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(49, 17);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Score:";
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.Location = new System.Drawing.Point(1189, 9);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(16, 17);
+            this.score.TabIndex = 1;
+            this.score.Text = "0";
+            // 
+            // restart
+            // 
+            this.restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart.Location = new System.Drawing.Point(12, 789);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(101, 60);
+            this.restart.TabIndex = 2;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.RestartGame);
+            // 
+            // horse1
+            // 
+            this.horse1.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse1.FlatAppearance.BorderSize = 0;
+            this.horse1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse1.Location = new System.Drawing.Point(110, 150);
+            this.horse1.Name = "horse1";
+            this.horse1.Size = new System.Drawing.Size(60, 60);
+            this.horse1.TabIndex = 3;
+            this.horse1.UseVisualStyleBackColor = false;
+            // 
+            // horse2
+            // 
+            this.horse2.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse2.FlatAppearance.BorderSize = 0;
+            this.horse2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse2.Location = new System.Drawing.Point(260, 100);
+            this.horse2.Name = "horse2";
+            this.horse2.Size = new System.Drawing.Size(60, 60);
+            this.horse2.TabIndex = 4;
+            this.horse2.UseVisualStyleBackColor = false;
+            // 
+            // horse3
+            // 
+            this.horse3.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse3.FlatAppearance.BorderSize = 0;
+            this.horse3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse3.Location = new System.Drawing.Point(360, 100);
+            this.horse3.Name = "horse3";
+            this.horse3.Size = new System.Drawing.Size(60, 60);
+            this.horse3.TabIndex = 5;
+            this.horse3.UseVisualStyleBackColor = false;
+            // 
+            // horse4
+            // 
+            this.horse4.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse4.FlatAppearance.BorderSize = 0;
+            this.horse4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse4.Location = new System.Drawing.Point(210, 250);
+            this.horse4.Name = "horse4";
+            this.horse4.Size = new System.Drawing.Size(60, 60);
+            this.horse4.TabIndex = 6;
+            this.horse4.UseVisualStyleBackColor = false;
+            // 
+            // horse5
+            // 
+            this.horse5.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse5.FlatAppearance.BorderSize = 0;
+            this.horse5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse5.Location = new System.Drawing.Point(310, 250);
+            this.horse5.Name = "horse5";
+            this.horse5.Size = new System.Drawing.Size(60, 60);
+            this.horse5.TabIndex = 7;
+            this.horse5.UseVisualStyleBackColor = false;
+            // 
+            // horse6
+            // 
+            this.horse6.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse6.FlatAppearance.BorderSize = 0;
+            this.horse6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse6.Location = new System.Drawing.Point(360, 300);
+            this.horse6.Name = "horse6";
+            this.horse6.Size = new System.Drawing.Size(60, 60);
+            this.horse6.TabIndex = 8;
+            this.horse6.UseVisualStyleBackColor = false;
+            // 
+            // horse7
+            // 
+            this.horse7.BackColor = System.Drawing.Color.SandyBrown;
+            this.horse7.FlatAppearance.BorderSize = 0;
+            this.horse7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
+            this.horse7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
+            this.horse7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.horse7.Location = new System.Drawing.Point(460, 200);
+            this.horse7.Name = "horse7";
+            this.horse7.Size = new System.Drawing.Size(60, 60);
+            this.horse7.TabIndex = 9;
+            this.horse7.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.horse7);
+            this.Controls.Add(this.horse6);
+            this.Controls.Add(this.horse5);
+            this.Controls.Add(this.horse4);
+            this.Controls.Add(this.horse3);
+            this.Controls.Add(this.horse2);
+            this.Controls.Add(this.horse1);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.scoreLabel);
+            this.Name = "Form1";
+            this.Text = "Lasso Game";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Button horse1;
+        private System.Windows.Forms.Button horse2;
+        private System.Windows.Forms.Button horse3;
+        private System.Windows.Forms.Button horse4;
+        private System.Windows.Forms.Button horse5;
+        private System.Windows.Forms.Button horse6;
+        private System.Windows.Forms.Button horse7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LassoGame
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        Button[] lasso;
+        bool reversed = false;
+        private enum Dir : byte { BottomRight, BottomLeft, TopLeft, TopRight, }
+        private Dir[] lassoDir = new Dir[12];
+        public Point lassoStartPos = new Point(760, 100);
+
+        public void CreateLasso()
+        {
+            lasso = new Button[12];
+            for (int i = 0; i < lasso.Length; ++i)
+            {
+                Button lassoPart = new Button();
+
+                lassoPart.FlatStyle = FlatStyle.Flat;
+                lassoPart.FlatAppearance.BorderSize = 0;
+                lassoPart.Tag = i;
+                lassoPart.SetBounds(lassoStartPos.X + 50 * i, lassoStartPos.Y + 50 * i, 60, 60);
+
+                if (i < 5 || i == 6)
+                    lassoPart.BackColor = lassoPart.FlatAppearance.MouseDownBackColor =
+                        lassoPart.FlatAppearance.MouseOverBackColor = SystemColors.ControlDark;
+                else
+                    lassoPart.BackColor = lassoPart.FlatAppearance.MouseDownBackColor =
+                        lassoPart.FlatAppearance.MouseOverBackColor = SystemColors.ActiveCaption;
+
+                lasso[i] = lassoPart;
+                Controls.Add(lasso[i]);
+                lasso[i].BringToFront();
+                lasso[i].Click += LassoClick;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            CreateLasso();
+        }
+
+        private void LassoClick(object sender, EventArgs e)
+        {
+            ++counter;
+            score.Text = Convert.ToString(counter);
+
+            Button lassoPart = sender as Button;
+            int ind = Convert.ToInt32(lassoPart.Tag);
+            int colour = lassoPart.BackColor == SystemColors.ControlDark ? 1 : -1;
+            int offset = Math.Abs(lasso[1].Top - lasso[0].Top);
+
+            if (!reversed)
+                for (int i = ind + 1; i < lasso.Length; i++)
+                    switch (lassoDir[i - 1])
+                    {
+                        case Dir.BottomRight:
+                            {
+                                lasso[i].Left = lasso[i - 1].Left - colour * offset;
+                                lasso[i].Top = lasso[i - 1].Top + colour * offset;
+                                lassoDir[i - 1] = colour == 1 ? Dir.BottomLeft : Dir.TopRight;
+                                break;
+                            }
+                        case Dir.BottomLeft:
+                            {
+                                lasso[i].Left = lasso[i - 1].Left - colour * offset;
+                                lasso[i].Top = lasso[i - 1].Top - colour * offset;
+                                lassoDir[i - 1] = colour == 1 ? Dir.TopLeft : Dir.BottomRight;
+                                break;
+                            }
+                        case Dir.TopLeft:
+                            {
+                                lasso[i].Left = lasso[i - 1].Left + colour * offset;
+                                lasso[i].Top = lasso[i - 1].Top - colour * offset;
+                                lassoDir[i - 1] = colour == 1 ? Dir.TopRight : Dir.BottomLeft;
+                                break;
+                            }
+                        case Dir.TopRight:
+                            {
+                                lasso[i].Left = lasso[i - 1].Left + colour * offset;
+                                lasso[i].Top = lasso[i - 1].Top + colour * offset;
+                                lassoDir[i - 1] = colour == 1 ? Dir.BottomRight : Dir.TopLeft;
+                                break;
+                            }
+                    }
+            else
+            {
+                colour *= -1;
+                for (int i = ind - 1; i >= 0; i--)
+                    switch ((Dir)lassoDir[i])
+                    {
+                        case Dir.BottomRight:
+                            {
+                                lasso[i].Left = lasso[i + 1].Left - colour * offset;
+                                lasso[i].Top = lasso[i + 1].Top + colour * offset;
+                                lassoDir[i] = colour == 1 ? Dir.TopRight : Dir.BottomLeft;
+                                break;
+                            }
+                        case Dir.BottomLeft:
+                            {
+                                lasso[i].Left = lasso[i + 1].Left - colour * offset;
+                                lasso[i].Top = lasso[i + 1].Top - colour * offset;
+                                lassoDir[i] = colour == 1 ? Dir.BottomRight : Dir.TopLeft;
+                                break;
+                            }
+                        case Dir.TopLeft:
+                            {
+                                lasso[i].Left = lasso[i + 1].Left + colour * offset;
+                                lasso[i].Top = lasso[i + 1].Top - colour * offset;
+                                lassoDir[i] = colour == 1 ? Dir.BottomLeft : Dir.TopRight;
+                                break;
+                            }
+                        case Dir.TopRight:
+                            {
+                                lasso[i].Left = lasso[i + 1].Left + colour * offset;
+                                lasso[i].Top = lasso[i + 1].Top + colour * offset;
+                                lassoDir[i] = colour == 1 ? Dir.TopLeft : Dir.BottomRight;
+                                break;
+                            }
+
+                    }
+            }
+        }
+
+        private void RestartGame(object sender, EventArgs e)
+        {
+            reversed = false;
+            score.Text = Convert.ToString(0);
+            counter = 0;
+            for (int i = 0; i < lassoDir.Length; ++i)
+            {
+                lassoDir[i] = Dir.BottomRight;
+            }
+            for (int i = 0; i < lasso.Length; i++)
+            {
+                lasso[i].BringToFront();
+            }
+            for (int i = 0; i < lasso.Length; i++)
+            {
+                lasso[i].Left = lassoStartPos.X + 50 * i;
+                lasso[i].Top = lassoStartPos.Y + 50 * i;
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (!reversed)
+                for (int i = lasso.Length - 1; i >= 0; --i)
+                {
+                    lasso[i].BringToFront();
+                }
+            else
+                for (int i = 0; i < lasso.Length; ++i)
+                {
+                    lasso[i].BringToFront();
+                }
+            reversed = !reversed;
+        }
+    }
+}
