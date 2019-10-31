1c99ba6ef25e45a92b35967d1e1d21235f5cc380
Daniel White <xt1zer@outlook.com>
2019-10-31 21:50:37 +0300

    Removed horse buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,6 @@
             this.scoreLabel = new System.Windows.Forms.Label();
             this.score = new System.Windows.Forms.Label();
             this.restart = new System.Windows.Forms.Button();
-            this.horse1 = new System.Windows.Forms.Button();
-            this.horse2 = new System.Windows.Forms.Button();
-            this.horse3 = new System.Windows.Forms.Button();
-            this.horse4 = new System.Windows.Forms.Button();
-            this.horse5 = new System.Windows.Forms.Button();
-            this.horse6 = new System.Windows.Forms.Button();
-            this.horse7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // scoreLabel
@@ -71,109 +64,11 @@
             this.restart.UseVisualStyleBackColor = true;
             this.restart.Click += new System.EventHandler(this.RestartGame);
             // 
-            // horse1
-            // 
-            this.horse1.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse1.FlatAppearance.BorderSize = 0;
-            this.horse1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse1.Location = new System.Drawing.Point(110, 150);
-            this.horse1.Name = "horse1";
-            this.horse1.Size = new System.Drawing.Size(60, 60);
-            this.horse1.TabIndex = 3;
-            this.horse1.UseVisualStyleBackColor = false;
-            // 
-            // horse2
-            // 
-            this.horse2.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse2.FlatAppearance.BorderSize = 0;
-            this.horse2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse2.Location = new System.Drawing.Point(260, 100);
-            this.horse2.Name = "horse2";
-            this.horse2.Size = new System.Drawing.Size(60, 60);
-            this.horse2.TabIndex = 4;
-            this.horse2.UseVisualStyleBackColor = false;
-            // 
-            // horse3
-            // 
-            this.horse3.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse3.FlatAppearance.BorderSize = 0;
-            this.horse3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse3.Location = new System.Drawing.Point(360, 100);
-            this.horse3.Name = "horse3";
-            this.horse3.Size = new System.Drawing.Size(60, 60);
-            this.horse3.TabIndex = 5;
-            this.horse3.UseVisualStyleBackColor = false;
-            // 
-            // horse4
-            // 
-            this.horse4.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse4.FlatAppearance.BorderSize = 0;
-            this.horse4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse4.Location = new System.Drawing.Point(210, 250);
-            this.horse4.Name = "horse4";
-            this.horse4.Size = new System.Drawing.Size(60, 60);
-            this.horse4.TabIndex = 6;
-            this.horse4.UseVisualStyleBackColor = false;
-            // 
-            // horse5
-            // 
-            this.horse5.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse5.FlatAppearance.BorderSize = 0;
-            this.horse5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse5.Location = new System.Drawing.Point(310, 250);
-            this.horse5.Name = "horse5";
-            this.horse5.Size = new System.Drawing.Size(60, 60);
-            this.horse5.TabIndex = 7;
-            this.horse5.UseVisualStyleBackColor = false;
-            // 
-            // horse6
-            // 
-            this.horse6.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse6.FlatAppearance.BorderSize = 0;
-            this.horse6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse6.Location = new System.Drawing.Point(360, 300);
-            this.horse6.Name = "horse6";
-            this.horse6.Size = new System.Drawing.Size(60, 60);
-            this.horse6.TabIndex = 8;
-            this.horse6.UseVisualStyleBackColor = false;
-            // 
-            // horse7
-            // 
-            this.horse7.BackColor = System.Drawing.Color.SandyBrown;
-            this.horse7.FlatAppearance.BorderSize = 0;
-            this.horse7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SandyBrown;
-            this.horse7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SandyBrown;
-            this.horse7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.horse7.Location = new System.Drawing.Point(460, 200);
-            this.horse7.Name = "horse7";
-            this.horse7.Size = new System.Drawing.Size(60, 60);
-            this.horse7.TabIndex = 9;
-            this.horse7.UseVisualStyleBackColor = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1584, 861);
-            this.Controls.Add(this.horse7);
-            this.Controls.Add(this.horse6);
-            this.Controls.Add(this.horse5);
-            this.Controls.Add(this.horse4);
-            this.Controls.Add(this.horse3);
-            this.Controls.Add(this.horse2);
-            this.Controls.Add(this.horse1);
             this.Controls.Add(this.restart);
             this.Controls.Add(this.score);
             this.Controls.Add(this.scoreLabel);
@@ -190,13 +85,6 @@
         private System.Windows.Forms.Label scoreLabel;
         private System.Windows.Forms.Label score;
         private System.Windows.Forms.Button restart;
-        private System.Windows.Forms.Button horse1;
-        private System.Windows.Forms.Button horse2;
-        private System.Windows.Forms.Button horse3;
-        private System.Windows.Forms.Button horse4;
-        private System.Windows.Forms.Button horse5;
-        private System.Windows.Forms.Button horse6;
-        private System.Windows.Forms.Button horse7;
     }
 }
 
